3a2e5079ba4878a7bcfd063813356554c81437c6
AnActualEmerald <kevinliger13@gmail.com>
2020-03-06 11:36:20 -0500

    Finished the basic app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,35 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 import { CharEditorComponent } from './char-editor/char-editor.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { CharListComponent } from './char-list/char-list.component';
+import { CharDetailsComponent } from './char-details/char-details.component';
 
+
+const routes: Routes = [
+  { path: '',
+    redirectTo: '/list',
+    pathMatch: 'full'
+  },
+  {path: 'edit', component: CharEditorComponent, data: {title: 'Character Editor'} },
+  {path: 'list', component: CharListComponent, data: {title: 'Character List'}},
+  {path: 'characters/:name', component: CharDetailsComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharEditorComponent
+    CharEditorComponent,
+    CharListComponent,
+    CharDetailsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes, {enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
